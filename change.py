4ad4c35480130bf46ebe250620d4fa2e29b42af5
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-10-05 20:26:01 -0700

    commit

---
+++ change.py
@@ -0,0 +1,12 @@
+print("Please enter an amount less than a dollar")
+value = int(input())
+quarters = value//25
+dimes = (value % 25)//10
+nickels = ((value % 25) % 10)//5
+pennies = (((value % 25) % 10) % 5)
+print("Your change will be:")
+print("Your change will be:")
+print("Q:", quarters)
+print("D:", dimes)
+print("N", nickels)
+print("P", pennies)
